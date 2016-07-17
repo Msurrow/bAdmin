6193d928b93fa0fedaf7607f8bae38cb86e4023b
Mark Surrow <msurrow@gmail.com>
2016-07-17 15:28:41 +0300

    Initial commit

---
+++ bAdmin_main.py
@@ -0,0 +1,273 @@
+from flask import Flask, jsonify, request, abort, make_response
+from datetime import datetime
+
+app = Flask(__name__)
+
+database = {"brugere": [{"id": 0, "name": "Anton", "clubs": [], "email": "", "phone": 12345678}, {"id": 1, "name": "Huggo", "clubs": [], "email": "", "phone": 12345678}, {"id": 2, "name": "Træner Kvinde", "clubs": [0, 1], "email": "", "phone": 12345678}, {"id": 3, "name": "Træner Mand", "clubs": [0], "email": "", "phone": 12345678}], 
+            "klubber": [{"id": 0, "name": "FooKlub", "admins": [], "coaches": [], "membershipRequests": []}, {"id": 1, "name": "BarKlub", "admins": [1], "coaches": [1], "membershipRequests": []}],
+            "traeningspas": [{"id": 0, "club": 0, "startTime": datetime(2016, 12, 24, 18, 00, 00), "durationMinutes": 120, "invited": [0, 1], "confirmed": [0]}]}
+
+@app.route("/")
+def index():
+    return "Velkommen til bAdmin portalen!"
+"""
+Brugere
+Bruger datatype:
+{
+    id: <int>, not null
+    name: <string>, not null
+    clubs: <list:int>, int must be id of existing Klub
+    email: <string>, valid email
+    phone: <int:8>, 8-digits valid DK phonenumber
+}
+"""
+@app.route("/brugere", methods=['GET', 'POST'])
+def users():
+    if request.method == 'POST':
+        if not request.json or 'name' not in request.json or len(request.json['name']) < 1:
+            abort(400)
+        else:
+            # A user is created without a club, as they have to apply for
+            # membership (ie. access to) to a club
+            bruger = {"id": database["brugere"][-1]["id"]+1,
+                      "name": request.json['name'],
+                      "clubs": [],
+                      "email": request.json['email'],
+                      "phone": request.json['phone']}
+
+            database["brugere"].append(bruger)
+
+            return jsonify(bruger)
+    # GET
+    else:
+        return jsonify(database["brugere"])
+
+@app.route("/brugere/<int:userId>", methods=['GET'])
+def user(userId):
+    bruger = [bruger for bruger in database["brugere"] if bruger["id"] == userId]
+    if len(bruger) == 0:
+        abort(404)
+
+    return jsonify(bruger[0])
+
+"""
+Klubber
+Klub datatype:
+{
+    id: <int>, not null
+    name: <string>, not null
+    admins: <list:int>, not empty, int must be id of existing Bruger
+    coaches: <list:int>, int must be id of existing Bruger
+    membershipRequests: <list:int, int must be id of existing Bruger
+}
+"""
+@app.route("/klubber", methods=['GET', 'POST'])
+def clubs():
+    if request.method == 'POST':
+        if not request.json or 'name' not in request.json or len(request.json['name']) < 1:
+            abort(400)
+
+        # Check request have admin list with atleast one admin. Validate
+        # user(s) for admin(s) exists
+        if 'admins' in request.json and isinstance(request.json['admins'], list) and len(request.json['admins']) > 0:
+            if not doesAllUsersInListExist(request.json['admins']):
+                abort(400)
+        else:
+            abort(400)
+
+        # A club is created without members and coaches, and defaults the admin
+        # to the creating user.
+        klub = {"id": database["klubber"][-1]["id"]+1,
+                "name": request.json['name'],
+                "admins": request.json['admins'],
+                "coaches": [],
+                "membershipRequests": []}
+
+        database["klubber"].append(klub)
+
+        return jsonify(klub)
+    # GET
+    else:
+        return jsonify(database["klubber"])
+
+@app.route("/klubber/<int:clubId>", methods=['GET', 'PUT'])
+def club(clubId):
+    # Does the club exist?
+    klub = [klub for klub in database['klubber'] if klub['id'] == clubId]
+    if len(klub) == 0:
+        abort(404)
+
+    if request.method == 'PUT':
+        if not request.json:
+            abort(400)
+
+        # Are we updating name? If so, validate and update
+        if 'name' in request.json:
+            # Don't allow empty name
+            if request.json['name'] is "" or len(request.json['name']) < 1:
+                abort(400)
+
+        # Are we updating admins list? If so, validate and update. Overwrite
+        # existing with input
+        if 'admins' in request.json:
+            # A club must have at least one admin, and all admins in list are
+            # users
+            if not isinstance(request.json['admins'], list) or len(request.json['admins']) < 1 or not doesAllUsersInListExist(request.json['admins']):
+                abort(400)
+
+        # Are we updating coaches list? If so, validate and update. Overwrite
+        # existing with input
+        if 'coaches' in request.json:
+            # Check all coaches in list are users
+            if not isinstance(request.json['coaches'], list) or not doesAllUsersInListExist(request.json['coaches']):
+                abort(400)
+
+        # Are we updating membership requests list? If so, validate and update.
+        # Overwrite existing with input
+        if 'membershipRequests' in request.json:
+            # Check all requests are from actual users
+            if not isinstance(request.json['membershipRequests'], list) or not doesAllUsersInListExist(request.json['membershipRequests']):
+                abort(400)
+
+        klub[0]['name'] = request.json['name']
+        klub[0]['admins'] = request.json['admins']
+        klub[0]['coaches'] = request.json['coaches']
+        klub[0]['membershipRequests'] = request.json['membershipRequests']
+
+        return jsonify(klub[0])
+    # GET
+    else:
+        return jsonify(klub[0])
+
+"""
+Træningspas
+Træningspas datatype:
+{
+    id: <int>, not null
+    club: <int>, not null club must be id of existing club
+    startTime: <datetime>, not null
+    durationMinutes: <int>, int must be >0
+    invited: <list:int>, ints must be id of existing Spiller
+    confirmed: <list:int>, ints must be id of existing Spiller
+}
+"""
+@app.route("/traeningspas", methods=['GET', 'POST'])
+def practices():
+    if request.method == 'POST':
+        if not request.json:
+            abort(400)
+
+        # Validate club exists and starttime is valid
+        if 'club' in request.json:
+            klub = [klub for klub in database['klubber'] if klub['id'] == request.json['club']]
+            if len(klub) == 0:
+                abort(404)
+        else:
+            abort(400)
+
+        # Check startTime exists and is valid
+        if 'startTime' not in request.json or not datetime(request.json['startTime']):
+            abort(400)
+
+        # Check durationMinutes exists and is valid
+        if 'durationMinutes' not in request.json or not isinstance(request.json['durationMinutes'], int) or request.json['durationMinutes'] <= 0:
+            abort(400)
+
+        # Check invited players are existing users
+        if 'invited' not in request.json or not isinstance(request.json['invited'], list) or not doesAllUsersInListExist(request.json['invited']):
+            abort(400)
+
+        # Check confirmed players are existing users
+        if 'confirmed' not in request.json or not doesAllUsersInListExist(request.json['confirmed']):
+            abort(400)
+
+        # A practice is created without confirmed invitees
+        traeningspas = {"id": database["traeningspas"][-1]["id"]+1,
+                        "club": request.json['club'],
+                        "startTime": datetime(request.json['startTime']),
+                        "durationMinutes": request.json['durationMinutes'],
+                        "invited": request.json['invited'],
+                        "confirmed": []}
+
+        database["traeningspas"].append(traeningspas)
+
+        return jsonify(traeningspas)
+    # GET
+    else:
+        return jsonify(database["traeningspas"])
+
+@app.route("/traeningspas/<int:practiceId>", methods=['GET', 'PUT'])
+def practice(practiceId):
+    # Does the træningspas exist?
+    traeningspas = [traeningspas for traeningspas in database['træningspas'] if traeningspas['id'] == practiceId]
+    if len(traeningspas) == 0:
+        abort(404)
+
+    if request.method == 'PUT':
+        if not request.json:
+            abort(400)
+        # Design decision: cannot updated clubId.
+
+        # Are we updating start time? If so, validate and update
+        if 'startTime' in request.json:
+            # Start time must be a valid date
+            if datetime(request.json['startTime']):
+                traeningspas[0]['startTime'] = datetime(request.json['startTime'])
+            else:
+                abort(400)
+
+        # Are we updating traeningspas duration? If so, validate and update.
+        if 'durationMinutes' in request.json:
+            # A duration must be >0 minutes
+            if isinstance(request.json['durationMinutes'], int) and request.json['durationMinutes'] <= 0:
+                traeningspas[0]['durationMinutes'] = request.json['durationMinutes']
+            else:
+                abort(400)
+
+        # Are we updating invited players list? If so, validate and update.
+        # Overwrite existing with input
+        if 'invited' in request.json:
+            # Check all players in list are users
+            if isinstance(request.json['invited'], list) and doesAllUsersInListExist(request.json['invited']):
+                traeningspas[0]['invited'] = request.json['invited']
+            else:
+                abort(400)
+
+        # Are we updating confirmed players list? If so, validate and update.
+        # Overwrite existing with input
+        if 'confirmed' in request.json:
+            # Check all confirmed players are actual users
+            if isinstance(request.json['confirmed'], list) and doesAllUsersInListExist(request.json['confirmed']):
+                traeningspas[0]['confirmed'] = request.json['confirmed']
+            else:
+                abort(400)
+
+        return jsonify(traeningspas[0])
+    # GET
+    else:
+        return jsonify(traeningspas[0])
+
+"""
+Helpers and error handlers
+"""
+def doesUserExist(userId):
+    bruger = [bruger for bruger in database["brugere"] if bruger["id"] == userId]
+    if len(bruger) == 0:
+        return False
+    else:
+        return True
+
+def doesAllUsersInListExist(userIDList):
+    if not isinstance(userIDList, list):
+        app.logger.warning("Called doesAllUsersInListExist with param that is not a list: ", userIDList)
+    for bruger in userIDList:
+        if not doesUserExist(bruger):
+            return False
+    return True
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
---
+++ bAdmin_unittests.py
@@ -0,0 +1,182 @@
+import bAdmin_main
+import unittest
+import json
+
+class bAdminTestUsers(unittest.TestCase):
+    def setUp(self):
+        bAdmin_main.app.config['TESTING'] = True
+        self.app = bAdmin_main.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_users_GET(self):
+        res = self.app.get("/brugere")
+        jres = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(jres[0]['id'],0)
+        self.assertEqual(jres[0]['name'],"Anton")
+
+    def test_users_POST_nodata(self):
+        res = self.app.post("/brugere", data=json.dumps({}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_users_POST_userHasName(self):
+        res = self.app.post("/brugere", data=json.dumps({'name_wrong':""}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_users_POST_userHasNameNotNothing(self):
+        res = self.app.post("/brugere", data=json.dumps({'name':""}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_users_POST_userIsCreated(self):
+        res1 = self.app.get("/brugere")
+        jres1 = json.loads(res1.data.decode())
+        lastUser = jres1[-1]['id']
+        self.assertEqual(lastUser, 3)
+
+        name = "Foobar"
+        email = "foo@gmail.com"
+        phone = "11223344"
+        clubs = "[1,2]"
+
+        res2 = self.app.post("/brugere", data=json.dumps({'name':name, 'email':email, 'phone':phone, 'clubs':clubs}), content_type='application/json')
+        jres2 = json.loads(res2.data.decode())
+        lastUser2 = jres2
+        self.assertEqual(lastUser2["id"], lastUser+1)
+        self.assertEqual(lastUser2["name"], name)
+        self.assertEqual(lastUser2["email"], email)
+        self.assertEqual(len(lastUser2["clubs"]), 0) # A user is created with 0 clubs and input arg is ignored
+
+    def test_user_GET_userExists(self):
+        res = self.app.get("/brugere/0")
+        jres = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(jres['id'],0)
+
+    def test_user_GET_userDoesntExist(self):
+        res = self.app.get("/brugere/5")
+        self.assertEqual(res.status_code, 404)
+
+class bAdminTestClubs(unittest.TestCase):
+    def setUp(self):
+        bAdmin_main.app.config['TESTING'] = True
+        self.app = bAdmin_main.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_clubs_GET(self):
+        res = self.app.get("/klubber")
+        jres = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(jres),2)
+
+    def test_clubs_POST_nodata(self):
+        res = self.app.post("/klubber", data=json.dumps({}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_clubs_POST_clubHasNameNotNothing(self):
+        res = self.app.post("/klubber", data=json.dumps({'name':""}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_clubs_POST_clubHasAdminlist(self):
+        res = self.app.post("/klubber", data=json.dumps({'admin_wrong':[]}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_clubs_POST_clubHasAdminlistNotEmpty(self):
+        res = self.app.post("/klubber", data=json.dumps({'admin':[]}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_clubs_POST_clubHasAdminlistUsersExist(self):
+        res = self.app.post("/klubber", data=json.dumps({'admin':[99]}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_clubs_POST_clusIsCreated(self):
+        res1 = self.app.get("/klubber")
+        jres1 = json.loads(res1.data.decode())
+        c1 = jres1[-1]['id']
+        self.assertEqual(c1, 1)
+
+        name = "Foobar"
+        admins = [0]
+        coaches = [1,2]
+        membershipRequests = [1,2]
+
+        res2 = self.app.post("/klubber", data=json.dumps({'name':name, 'admins':admins, 'coaches':coaches, 'membershipRequests':membershipRequests}), content_type='application/json')
+        jres2 = json.loads(res2.data.decode())
+        c2 = jres2
+        self.assertEqual(c2["id"], c1+1)
+        self.assertEqual(c2["name"], name)
+        self.assertEqual(c2["admins"], admins)
+
+        self.assertEqual(len(c2["coaches"]), 0) # A bluc is created with 0 coaches and input arg is ignored
+        self.assertEqual(len(c2["membershipRequests"]), 0) # A bluc is created with 0 membershiprequests and input arg is ignored
+
+    def test_club_GET_clubExists(self):
+        res = self.app.get("/klubber/0")
+        jres = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(jres['id'],0)
+
+    def test_club_GET_clubDoesntExist(self):
+        res = self.app.get("/klubber/99")
+        self.assertEqual(res.status_code, 404)
+
+    def test_club_PUT_hasData(self):
+        res = self.app.put("/klubber/0", data=json.dumps({}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_club_PUT_nameIsValid(self):
+        res = self.app.put("/klubber/0", data=json.dumps({'name':""}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_club_PUT_adminsIsValid(self):
+        res = self.app.put("/klubber/0", data=json.dumps({'admins': [99]}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+        res = self.app.put("/klubber/0", data=json.dumps({'admins': []}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+        res = self.app.put("/klubber/0", data=json.dumps({'admins': 'notalist'}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_club_PUT_coachesIsValid(self):
+        res = self.app.put("/klubber/0", data=json.dumps({'coaches': [99]}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+        res = self.app.put("/klubber/0", data=json.dumps({'coaches': 'notalist'}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_club_PUT_membershipRequestsIsValid(self):
+        res = self.app.put("/klubber/0", data=json.dumps({'membershipRequests': [99]}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+        res = self.app.put("/klubber/0", data=json.dumps({'membershipRequests': 'notalist'}), content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_club_PUT_clubIsUpdated(self):
+        res = self.app.get("/klubber/0")
+        c = json.loads(res.data.decode())
+
+        name = "New Name"
+        admins = [0]
+        coaches = [0,1]
+        membershipRequests = [0]
+        res2 = self.app.put("/klubber/0", data=json.dumps({'name': name,
+                                                           'admins': admins,
+                                                           'coaches': coaches,
+                                                           'membershipRequests': membershipRequests}), content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+        c2 = json.loads(res2.data.decode())
+        self.assertEqual(c2['name'], name)
+        self.assertEqual(c2['admins'], admins)
+        self.assertEqual(c2['coaches'], coaches)
+        self.assertEqual(c2['membershipRequests'], membershipRequests)
+
+class bAdminTestPractices(unittest.TestCase):
+    def setUp(self):
+        bAdmin_main.app.config['TESTING'] = True
+        self.app = bAdmin_main.app.test_client()
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
