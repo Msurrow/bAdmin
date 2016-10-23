4fb2bb85de937d758dc9a66d542a769fe802bba6
Mark Surrow <msurrow@gmail.com>
2016-10-23 18:26:26 +1300

    debugging timestamps

--- static/js/bAdminAPIService.js
+++ static/js/bAdminAPIService.js
@@ -167,7 +167,11 @@ bAdminAPIService.factory('bAdminAPI', ['$q', '$log', '$http', 'gatekeeper', func
         //If name is left empty just use a default
         var practiceName = newPracticeName === "" ? "Træningspas" : newPracticeName;
         //Parse dato into ISO8601 YYYY-MM-DD HH:MMZZ. Asumes datepicker format option is set correctly.
-        var practiceStartTime = moment(newPracticeDate + "T" + newPracticeStartHour + ":" + newPracticeStartMinute).format("YYYY-MM-DDTHH:MMZZ").toString();
+        $log.debug("GOT TIME INPUT: "+newPracticeDate+"  "+newPracticeStartHour+"  "+newPracticeStartMinute)
+        var mdate = moment(newPracticeDate + "T" + newPracticeStartHour + ":" + newPracticeStartMinute).toString();
+        var practiceStartTime = mdate.format("YYYY-MM-DDTHH:MMZZ").toString();
+        $log.debug("MOMENT TIME:"+mdate)
+        $log.debug("MOMENT TIME FORMAT:"+practiceStartTime)
 
         var practiceDuration = newPracticeDuration;
         //We include repeats param and let the backend handle it, to avoid multiple requests
